1a5af3baded7d3f0399e2e8da694abacb2e8d887
marcorlino <marcangelo.orlino.s@bulsu.edu.ph>
2023-03-28 23:39:17 +0800

    test

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
-export class ProductDescriptionComponent {
+export class ProductDescriptionComponent implements OnInit
+{
+  albumInfo: any;
 
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit(): void {
+    this._productService.getAlbum(1).subscribe((response: any) => this.albumInfo = response);
+  }
 }
