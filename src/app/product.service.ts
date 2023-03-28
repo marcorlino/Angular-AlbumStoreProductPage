55ed3431efff078606dd22124b4bdd6dd1522676
marcorlino <marcangelo.orlino.s@bulsu.edu.ph>
2023-03-28 23:33:34 +0800

    edit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse} from '@angular/common/http';
-
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import 'rxjs/add/operator/map'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  private _albumUrl = '../assets/album.json';
+
   constructor(private _http: HttpClient) { }
+
+  getAlbum(id: number)
+  {
+    return this._http.get(this._albumUrl).map((response:any) => response.json());
+  }
 }
 
