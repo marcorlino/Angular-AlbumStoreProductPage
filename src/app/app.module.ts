2e26a18732faa0c71b9c97388e3eb62021251aed
marcorlino <marcangelo.orlino.s@bulsu.edu.ph>
2023-03-29 00:34:22 +0800

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
    { path: 'products', component: ProductListComponent },
-   { path: 'products/:id', component: ProductPageComponent },
+   { path: 'product/:id', component: ProductPageComponent },
   { path: '', redirectTo: 'products', pathMatch: 'full' }
 ]
 
@@ -31,7 +31,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
